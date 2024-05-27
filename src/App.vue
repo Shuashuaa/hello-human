0d9f7a10414f7efcfc96b28897e1a6e0c018530c
Shuashuaa <jovijoshuatania027@gmail.com>
2024-05-27 21:05:10 +0800

    052724_Boiletplate

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="App">
+		<h1 style="font-size: 77px;">hello there, Human!.</h1>
+		<button @click="changeName('zelda')">ChangeNAme</button>
+	</div>
+	
+	<router-view/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'App',
+	components: {},
+
+	data(){
+		return {
+			name: 'Link',
+			age: 18 as number | string
+		}
+	},
+	methods: {
+		changeName(name: string) {
+			this.name = name; 
+			alert(this.name)
+		},
+		changeAge(age: number){
+			this.age = age
+		}
+	}
+});
+</script>
+
+<style>
+
+</style>
+
+
+<!-- <template>
+	<div class="App">
+		<h1 style="font-size: 77px;">hello there, Human!.</h1>
+	</div>
+	
+	<router-view/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'App',
+	components: {}
+});
+</script>
+
+<style>
+
+</style> -->
