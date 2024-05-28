5cbd28ce2ebeea8b1ce8b7acd5ba10af74c02359
Shuashuaa <jovijoshuatania027@gmail.com>
2024-05-28 21:13:16 +0800

    052824_APP

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
 		<div class="header">  
 			<h1 style="font-size: 20px;">hello there, Human!</h1>
     	</div>
-		
-		
+		{{ name }} is {{ age }} years old
+		<button @click="changeName('Juswa')">Change</button>
 	</div>
 </template>
 
