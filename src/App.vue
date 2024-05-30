c5caf386843b90c827775e5a7cbf8e3b3e3211bc
Shuashuaa <jovijoshuatania027@gmail.com>
2024-05-30 20:28:54 +0800

    053024-refs

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,27 @@
 			<h1 style="font-size: 20px;">hello there, Human!</h1>
     	</div>
 		
-		<button @click="changeName('juswa'), changeAge('25')"></button>
-		{{ name }} - {{ age }}
+		<!-- <button @click="changeName('juswa'), changeAge('25')"></button> -->
+		<!-- {{ name }} - {{ age }} -->
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
 import HomeComp from './components/HomeComp.vue';
-import type Job from './types/Job';
 
 export default defineComponent({
 	name: 'App',
 	components: { HomeComp },
 
 	setup() {
-
-		/*
-			reactive 1
-		*/
-		const state = reactive({
-			name: 'Link',
-			age: 25 as string | number //Type Assertion
-		})
-
-		state.name = ''
-		state.age = 0
-		return { 
-			...toRefs(state)
-		}
-
 		/*
 			Refs 2
 		*/
-		// const name = ref('Link');
-		// const age = ref<number | string>(25); //Generic arguments
+		const name = ref('Link');
+		const age = ref<number | string>(25); //Generic arguments
 
-		// return { name, age }
+		return { name, age }
 	},
 
 	data(){
