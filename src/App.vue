df4e00593e62037eec969eaf241bec92968d121b
Shuashuaa <jovijoshuatania027@gmail.com>
2024-06-01 07:01:44 +0800

    060124-Types

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,40 @@
 		<div class="header">  
 			<h1 style="font-size: 20px;">hello there, Human!</h1>
     	</div>
-		
-		<button @click="changeName('juswa'), changeAge('25')"></button>
-		{{ name }} - {{ age }}
+	
+		<div v-for="job in jobs" :key="job.id">
+			<p>{{ job.title }} - {{ job.location }} - {{ job.salary }}</p>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
 import HomeComp from './components/HomeComp.vue';
+import type Job from './types/Job';
 
 export default defineComponent({
 	name: 'App',
 	components: { HomeComp },
 
 	setup() {
-		/*
-			Refs 2
-		*/
-		const name = ref('Link');
-		const age = ref<number | string>(25); //Generic arguments
 
-		return { name, age }
-	},
+		const jobs = ref<Job[]>([
+			{ title: 'Farm Worker', location: 'lon-lon ranch', salary: 10000, id: '1'},
+			{ title: 'Flavor Scientist', location: 'US based company', salary: 60000, id: '2'},
+			{ title: 'Coach', location: 'Somewhere near', salary: 15000, id: '3'},
+			{ title: 'Wood Cutter', location: 'Somewhere far', salary: 50000, id: '4'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'},
+			{ title: 'Grass Cleaner', location: 'house', salary: 80000, id: '5'}
+		])
 
-	data(){
-		return {
-			name: 'Josh',
-			age: 18 as string | number
-		}
-	},
-	methods: {
-		changeName(name: string){
-			this.name = name;
-			return name;
-		},
-		changeAge(age: string | number){
-			this.age = age;
-			return age;
-		}
+		return { jobs }
 	},
 });
 </script>
