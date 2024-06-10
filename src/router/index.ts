165706ccb2b5c059d837a2684c53d23fcafc45e4
Shuashuaa <jovijoshuatania027@gmail.com>
2024-06-10 18:45:08 +0800

    061024

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeComp from '../components/HomeComp.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), //deployment
-  // history: createWebHistory('/dist/'), //deployment
+  // history: createWebHistory(import.meta.env.BASE_URL), //deployment
+  history: createWebHistory('/opening/'), //deployment
   routes: [
     {
       path: '/',
@@ -11,6 +11,18 @@ const router = createRouter({
       component: HomeComp
     },
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.path !== '/opening'){
+    if(to.path == '/'){
+      next('/opening')
+    }else{
+      next()
+    }
+  }else{
+    next()
+  }
 })
 
 export default router
